6f51c544606d56e66a9ee92245e99e7658277d56
Lukas Jungmann <lukas.jungmann@oracle.com>
2018-09-19 15:01:31 +0200

    Initial contribution
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# WS-Policy implementation for Project Metro
+
+### Licensing and Governance
+
+JAXB API is licensed under a license - [EDL 1.0](LICENSE.md).
---
+++ policy/src/main/java/module-info.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2017, 2018 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * WS-Policy implementation for Metro
+ */
+module com.sun.xml.ws.policy {
+    requires transitive java.activation;
+    requires transitive java.xml;
+    requires com.sun.istack.runtime;
+    requires java.logging;
+    requires com.sun.xml.txw2;
+    requires java.annotation;
+
+    exports com.sun.xml.ws.policy.sourcemodel.wspolicy;
+    exports com.sun.xml.ws.policy.privateutil;
+    exports com.sun.xml.ws.policy;
+    exports com.sun.xml.ws.policy.sourcemodel;
+    exports com.sun.xml.ws.policy.spi;
+    exports com.sun.xml.ws.policy.subject;
+    exports com.sun.xml.ws.policy.util;
+}
